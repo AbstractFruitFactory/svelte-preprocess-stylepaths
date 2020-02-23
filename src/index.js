dae528b3b0a337a1615c1d852b72cecffe9193a8
Alexander Wormbs <alexmedkex@gmail.com>
2020-02-23 21:16:03 +0000

    add build and cleanup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const fs = require('fs')
+import fs from 'fs'
 import path from 'path'
 
 export default ({ content, attributes, filename }) => {
     if (!attributes.src) return
 
-    let stylesPath: string
+    let stylesPath
 
     if (path.isAbsolute(attributes.src)) {
         stylesPath = `${path.dirname(require.main.filename)}/../../../..${attributes.src}`
     } else {
         const normalized = path.normalize(attributes.src)
-        const pathArray: Array<string> = filename.split('/')
+        const pathArray = filename.split('/')
         pathArray.pop()
         pathArray.push(normalized)
         stylesPath = pathArray.join('/')
